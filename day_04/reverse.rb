cbbe1b7a1e579ca249ee9e4f9720c9a6f02a74f8
Dianna Tingg <dtingg@gmail.com>
2019-07-25 15:24:50 -0700

    added day 4 exercise reverse

---
+++ day_04/reverse.rb
@@ -0,0 +1,11 @@
+# Ada Jump Start Live - Day 4 Exercise
+
+puts "Demonstrate the array method for reverse!"
+
+my_array = ["Julia", "Erika", "Eve", "Elizabeth", "Dianna"]
+
+puts "\nThe original array is: #{my_array}"
+
+my_array.reverse!
+
+puts "\nThe array is now: #{my_array}"
